7b982e5ef750b5fcdd4a425bbc301b1a85741bfc
BredeFj <bredefjellanger@gmail.com>
2025-08-21 09:44:04 +0200

    bestcommit

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 #Oppgave: få terminal til å spørre om navnet og så
 #printe ut en velkomstmelding til dette navnet
 #prøv så å laste opp prosjektet på github
+
+
+print("sigma")
+
+#input
+name = input("what is your name")
+print("your name is" + name)
+
+age = input("how old are you")
+print("you are" + age + " år")
